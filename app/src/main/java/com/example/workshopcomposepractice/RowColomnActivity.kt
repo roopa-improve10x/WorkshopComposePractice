c6bde75db15fd0031889dd811280e421ec059ccb
roopa <roopa.peddinti@improve10x.com>
2024-02-08 12:44:19 +0530

    created a activity and completed the row and column methods

--- app/src/main/java/com/example/workshopcomposepractice/RowColomnActivity.kt
+++ app/src/main/java/com/example/workshopcomposepractice/RowColomnActivity.kt
@@ -3,20 +3,38 @@ package com.example.workshopcomposepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.workshopcomposepractice.ui.theme.WorkshopComposePracticeTheme
 
 class RowColomnActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Row(modifier = Modifier
+                .width(200.dp)
+                .height(300.dp)
+                .background(Color.LightGray),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically) {
+                Text("Hello")
+                Text("World!")
+                Text("Hello")
+            }
         }
     }
 }
